116df6db41cc1a53e5178091ed32bad23198a7f2
Mahekarim <mahekarim@gmail.com>
2019-12-04 04:26:29 +0600

    :wheelchair:  AdminMiddleware & Blade Directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // start from here
+        Blade::if('admin', function () {
+            return auth()->check() && auth()->user()->user_role_id == 1;
+        });
     }
 }
