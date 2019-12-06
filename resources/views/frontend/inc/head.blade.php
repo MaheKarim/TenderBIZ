0f3ad3097c441cda323d1199d6c2631b33036615
MaheKarim <mahekarim@gmail.com>
2019-12-06 23:59:05 +0600

    Update

--- resources/views/frontend/inc/head.blade.php
+++ resources/views/frontend/inc/head.blade.php
@@ -4,31 +4,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap Min CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/bootstrap.min.css">
     <!-- Animate Min CSS -->
-    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/animate.min.css">
     <!-- FontAwesome Min CSS -->
-    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/fontawesome.min.css">
     <!-- FlatIcon CSS -->
-    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/flaticon.css">
     <!-- Owl Carousel Min CSS -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/owl.carousel.min.css">
     <!-- Slick Min CSS -->
-    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/slick.min.css">
     <!-- MeanMenu CSS -->
-    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/meanmenu.css">
     <!-- Magnific Popup Min CSS -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/magnific-popup.min.css">
     <!-- Odometer Min CSS -->
-    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/odometer.min.css">
     <!-- Nice Select Min CSS -->
-    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/nice-select.min.css">
     <!-- Style CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/style.css">
     <!-- Responsive CSS -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('/') }}assets/css/responsive.css">
 
     <title>TenderBIZ - Drop Your Tender</title>
 
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="{{ asset('/') }}assets/img/favicon.png">
 </head>
--- resources/views/frontend/inc/script.blade.php
+++ resources/views/frontend/inc/script.blade.php
@@ -1,32 +1,32 @@
     <!-- jQuery Min JS -->
-    <script src="assets/js/jquery.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/jquery.min.js"></script>
     <!-- Popper Min JS -->
-    <script src="assets/js/popper.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/popper.min.js"></script>
     <!-- Bootstrap Min JS -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/bootstrap.min.js"></script>
     <!-- Parallax Min JS -->
-    <script src="assets/js/parallax.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/parallax.min.js"></script>
     <!-- Owl Carousel Min JS -->
-    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/owl.carousel.min.js"></script>
     <!-- Slick Min JS -->
-    <script src="assets/js/slick.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/slick.min.js"></script>
     <!-- MeanMenu JS -->
-    <script src="assets/js/jquery.meanmenu.js"></script>
+    <script src="{{ asset('/') }}assets/js/jquery.meanmenu.js"></script>
     <!-- Appear Min JS -->
-    <script src="assets/js/jquery.appear.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/jquery.appear.min.js"></script>
     <!-- Odometer Min JS -->
-    <script src="assets/js/odometer.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/odometer.min.js"></script>
     <!-- Nice Select Min JS -->
-    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/jquery.nice-select.min.js"></script>
     <!-- Magnific Popup Min JS -->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/jquery.magnific-popup.min.js"></script>
     <!-- WOW Min JS -->
-    <script src="assets/js/wow.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/wow.min.js"></script>
     <!-- AjaxChimp Min JS -->
-    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/jquery.ajaxchimp.min.js"></script>
     <!-- Form Validator Min JS -->
-    <script src="assets/js/form-validator.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/form-validator.min.js"></script>
     <!-- Contact Form Min JS -->
-    <script src="assets/js/contact-form-script.js"></script>
+    <script src="{{ asset('/') }}assets/js/contact-form-script.js"></script>
     <!-- Main JS -->
-    <script src="assets/js/main.js"></script>
+    <script src="{{ asset('/') }}assets/js/main.js"></script>
